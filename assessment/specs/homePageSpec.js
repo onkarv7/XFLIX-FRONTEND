e71392572b8ab146ce5f935ff87d5b34379357b1
Nabhan Abdulla <nabhan@criodo.com>
2021-03-04 13:45:18 +0550

    updated changes - 2021-03-04 13:25:18

--- assessment/specs/homePageSpec.js
+++ assessment/specs/homePageSpec.js
@@ -1,67 +1,71 @@
-import homePage from '../pages/homePage'
+import homePage from "../pages/homePage";
 
-describe('Search', () => {
+describe("XFlix", () => {
   beforeEach(() => {
-    homePage.goto()
-  })
+    homePage.goto();
+  });
 
-  it('should display 5 genres in row 1', () => {
-    homePage.genre().should('have.length', 5)
-  })
+  it("should display 5 genres in row 1", () => {
+    homePage.genre().should("have.length", 5);
+  });
 
-  it('should display 5 buttons on content rating', () => {
-    homePage.genre().should('have.length', 5)
-  })
+  it("should display 5 buttons on content rating", () => {
+    homePage.genre().should("have.length", 5);
+  });
 
-  it('Parent of genre and content ratings should not be same', () => {
-    homePage.genre().parent().should('to.not.equal', homePage.contentRating().parent());
-  })
+  it("should not have same parent for genre and content rating buttons", () => {
+    homePage
+      .genre()
+      .parent()
+      .should("to.not.equal", homePage.contentRating().parent());
+  });
 
-  it('Sort by view count is not clickable on page load', () => {
-    homePage.viewCount().should('not.to.be.selected');
-  })
+  it("should not have viewCount option selected in sort by dropdown on page load", () => {
+    homePage.viewCount().should("not.to.be.selected");
+  });
 
-  it('Sort by upload date should be selected on page load', () => {
-    homePage.releaseDate().should('to.be.selected');
-  })
+  it("should have releaseDate option selected in sort by dropdown on page load", () => {
+    homePage.releaseDate().should("to.be.selected");
+  });
 
-  it('When you click on sort by, view-count option becomes clickable and selectable', () => {
+  it("should have viewCount option selectable after clicking on the sort by dropdown", () => {
     homePage.sortBy().select("viewCount");
-    homePage.viewCount().should('to.be.selected');
-  })
+    homePage.viewCount().should("to.be.selected");
+  });
 
-  it('Make sure to have upload button on the page on load', () => {
-    homePage.uploadButton().should('to.have.length.of', 1)
-  })
+  it('should have "Upload" button on landing page on load', () => {
+    homePage.uploadButton().should("to.have.length.of", 1);
+  });
 
-  it('Click on upload button and make sure that the modal comes up. with submit and cancel button', () => {
+  it('should open video modal with "Submit" and "Cancel" buttons, on clicking "Upload" button', () => {
     homePage.modalSubmitButton().should("not.to.exist");
     homePage.uploadButton().click();
     homePage.modalCancelButton().focus();
     homePage.modalSubmitButton().should("to.be.visible");
     homePage.modalCancelButton().should("to.be.visible");
-  })
+  });
 
-  it('Click on upload button and make sure that ' +
-      'the modal comes up. with submit and cancel button ' +
-      'Click on cancel button and make sure that the modal disappears', () => {
+  it('should open video modal on clicking "Upload" button and close it on clicking "Cancel" button in the modal', () => {
     homePage.uploadButton().click();
     homePage.modalCancelButton().focus();
     homePage.modalCancelButton().should("to.be.visible");
     homePage.modalCancelButton().click();
     homePage.modalCancelButton().should("not.to.exist");
-  })
+  });
 
-  it('page should have more than 10 video-tiles-link on load.', () => {
-    homePage.videoTileLink().should('to.have.length.of.at.least', 10)
-  })
+  it('should have at least 10 links (with class "video-tiles-link") to different videos on page load.', () => {
+    homePage.videoTileLink().should("to.have.length.of.at.least", 10);
+  });
 
-  it('minimum video-tile\'s on page load. ' +
-      'randomly pickup one of them to verify if it contains video-tile-link in parents.', () => {
-    let videoTileList = homePage.videoTile();
-    videoTileList.should('to.have.length.of.at.least', 10)
-    videoTileList.first().click()
-    homePage.viewIframe().should('to.have.length.of', 1);
-  })
-})
+  it(
+    'should display the video page on clicking the first video tile (with class "video-tile") which has a parent element with class "video-tile"' +
+      'which is child of an "video-tile-link" element ',
+    () => {
+      let videoTileList = homePage.videoTile();
+      videoTileList.should("to.have.length.of.at.least", 10);
+      videoTileList.first().click();
+      homePage.viewIframe().should("to.have.length.of", 1);
+    }
+  );
+});
 
