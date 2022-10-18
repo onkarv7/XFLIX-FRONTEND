0c302d2a7a4709532b9a9ebeaa37da0df7f53633
Onkar Vasav <102866144+onkarv7@users.noreply.github.com>
2022-10-18 10:51:51 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,11 +11,11 @@ const endpoint = "https://8ea560cf-d096-4965-928f-07f0c06fe93b.mock.pstmn.io/v1/
 
 function App() {
   return (
+    
     <Router>
     <React.StrictMode>
     <div className="App">
       <Switch>
-      
       <Route path="/video/:id" >
         <Videopage />
       </Route>
@@ -23,22 +23,6 @@ function App() {
         <Main />
       </Route>
       </Switch>
-     
-      {/* <Header /> */}
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
     </React.StrictMode>
     </Router>
